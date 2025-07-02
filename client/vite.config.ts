474899188a5cc2cda7e78fbd419f884f94e333f0
Crississippi <crismalipico12@gmail.com>
2025-07-03 01:08:04 +0800

    Final Version 2.3

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
     proxy: {
       '/api': 'https://dj-app-4uc9.onrender.com'
